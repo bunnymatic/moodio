f54a48f6bf25a11ee7a0bcde2ca197563e4c37e7
Mr Rogers <jon@bunnymatic.com>
2014-10-16 23:23:35 -0700

    working

--- js/main.js
+++ js/main.js
@@ -16,26 +16,13 @@ $(function() {
   var mesh = new FSS.Mesh(geometry, material);
   scene.add(mesh);
 
-  setInterval(function() {
-    // Augment vertices for animation
-    var v, vertex;
-    for (v = geometry.vertices.length - 1; v >= 0; v--) {
-      vertex = geometry.vertices[v];
-      vertex.anchor = FSS.Vector3.clone(vertex.position);
-      vertex.step = FSS.Vector3.create(
-        Math.randomInRange(0.2, 1.0),
-        Math.randomInRange(0.2, 1.0),
-        Math.randomInRange(0.2, 1.0)
-      );
-      vertex.time = Math.randomInRange(0, Math.PIM2);
-    }
-
-
-    // 5) Create and add a Light to the Scene:
-    var light = new FSS.Light('#ccff00', '#0000FF');
-    scene.add(light);
-    // 6) Finally, render the Scene:
+  function animate() {
+    now = Date.now() - start;
+    light.setPosition(300*Math.sin(now*0.001), 200*Math.cos(now*0.0005), 60);
     renderer.render(scene);
-  },100);
+    requestAnimationFrame(animate);
+  }
+
+  animate()
 
 });
