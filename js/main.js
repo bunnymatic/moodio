8951df4e40a313705c5096017906f472f052d2ef
Mr Rogers <jon@bunnymatic.com>
2014-10-16 23:30:11 -0700

    get animation working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function() {
   // 1) Create a Renderer for the context you would like to render to.
   //    You can use either the WebGLRenderer, CanvasRenderer or SVGRenderer.
+  var now, start = Date.now();
   var renderer = new FSS.CanvasRenderer();
 
   // 2) Add the Renderer's element to the DOM:
@@ -11,17 +12,22 @@ $(function() {
   var scene = new FSS.Scene();
 
   // 4) Create some Geometry & a Material, pass them to a Mesh constructor, and add the Mesh to the Scene:
-  var geometry = new FSS.Plane(400, 400, 44, 2);
+  var geometry = new FSS.Plane(600, 400, 6, 4);
   var material = new FSS.Material('#444444', '#FFFFFF');
+  var light = new FSS.Light('#111122', '#FF0022');
   var mesh = new FSS.Mesh(geometry, material);
-  scene.add(mesh);
+
 
   function animate() {
     now = Date.now() - start;
+    geometry = new FSS.Plane(600 * Math.sin(now*0.02), 400, 6, 4);
     light.setPosition(300*Math.sin(now*0.001), 200*Math.cos(now*0.0005), 60);
     renderer.render(scene);
     requestAnimationFrame(animate);
   }
+
+  scene.add(mesh);
+  scene.add(light);
 
   animate()
 
