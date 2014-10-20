3ee66ced83e2104726c4a0f1c991709dada8a712
Mr Rogers <jon@bunnymatic.com>
2014-10-20 10:49:09 -0700

    add gulp and coffee script handling

---
+++ coffee/main.coffee
@@ -0,0 +1,109 @@
+$ -> 
+
+  now = null
+  
+  MESH = 
+    xRange: 0.2
+    yRange: 0.2
+    zRange: 0.01
+    width: 500
+    height: 500
+    depth: 10,
+    segments: 30
+    slices: 30
+    speed: 18
+
+
+
+  # 1) Create a Renderer for the context you would like to render to.
+  #    You can use either the WebGLRenderer, CanvasRenderer or SVGRenderer.
+
+  # 2) Add the Renderer's element to the DOM:
+
+  # 3) Create a Scene:
+
+  # 4) Create some Geometry & a Material, pass them to a Mesh constructor, and add the Mesh to the Scene:
+
+  # Set the Geometry to dirty
+  animate = ->
+    now = Date.now() - start
+    removeLights()
+    addLights()
+    adjustVertices()
+    renderer.render scene
+    requestAnimationFrame animate
+    return
+
+  initVertices = (geometry) ->
+    v = undefined
+    vertex = undefined
+    v = geometry.vertices.length - 1
+    while v >= 0
+      vertex = geometry.vertices[v]
+      vertex.anchor = FSS.Vector3.clone(vertex.position)
+      vertex.step = FSS.Vector3.create(0.001, 0.001, 0.001)
+      vertex.time = Math.randomInRange(0, Math.PIM2)
+      v--
+    return
+
+  now = undefined
+  start = Date.now()
+  numLights = 1
+  renderer = new FSS.CanvasRenderer()
+  container = document.getElementById("container")
+  container.appendChild renderer.element
+  scene = new FSS.Scene()
+  geometry = new FSS.Plane(MESH.width, MESH.height, MESH.segments, MESH.slices)
+  material = new FSS.Material("#442222", "#FFFF77")
+  mesh = new FSS.Mesh(geometry, material)
+  initVertices geometry
+  speed = ->
+    MESH.speed / 10000.0
+
+  removeLights = (lights) ->
+    lights = lights or scene.lights
+    _.each scene.lights, (light) ->
+      scene.remove light
+      return
+
+    return
+
+  addLights = ->
+    oldLights = scene.lights
+    _.times numLights, ->
+      ambient = ["#112222"][parseInt(now, 10) % 1]
+      diffuse = ["#ffccff"][parseInt(now, 10) % 1]
+      light = new FSS.Light(ambient, diffuse)
+      scene.add light
+      newPosition = [
+        MESH.width * Math.sin(now * speed()) / 4.0,
+        MESH.height * Math.cos(now * speed()) / 4.0,
+        MESH.depth * 2.0
+      ]
+      light.setPosition.apply(light,newPosition)
+      return
+
+    oldLights
+
+  adjustVertices = ->
+    offset = MESH.depth / 2
+    v = geometry.vertices.length - 1
+    while v >= 0
+      vertex = geometry.vertices[v]
+      ox = Math.sin(vertex.time + vertex.step[0] * now * speed())
+      oy = Math.cos(vertex.time + vertex.step[1] * now * speed())
+      oz = Math.sin(vertex.time + vertex.step[2] * now * speed())
+      FSS.Vector3.set(
+        vertex.position,
+        MESH.xRange * geometry.segmentWidth * ox,
+        MESH.yRange * geometry.sliceHeight * oy,
+        MESH.zRange * offset * oz - offset
+      )
+      FSS.Vector3.add vertex.position, vertex.anchor
+      v--
+    geometry.dirty = true
+    return
+
+  scene.add mesh
+  animate()
+  
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function() {
-
+  var MESH, addLights, adjustVertices, animate, container, geometry, initVertices, material, mesh, now, numLights, removeLights, renderer, scene, speed, start;
+  now = null;
   MESH = {
     xRange: 0.2,
     yRange: 0.2,
@@ -10,94 +11,77 @@ $(function() {
     segments: 30,
     slices: 30,
     speed: 18
-  }
-
-
-  var initVertices = function(geometry) {
+  };
+  animate = function() {
+    now = Date.now() - start;
+    removeLights();
+    addLights();
+    adjustVertices();
+    renderer.render(scene);
+    requestAnimationFrame(animate);
+  };
+  initVertices = function(geometry) {
     var v, vertex;
-    for (v = geometry.vertices.length - 1; v >= 0; v--) {
+    v = void 0;
+    vertex = void 0;
+    v = geometry.vertices.length - 1;
+    while (v >= 0) {
       vertex = geometry.vertices[v];
       vertex.anchor = FSS.Vector3.clone(vertex.position);
-      vertex.step = FSS.Vector3.create(0,0,0)
-
+      vertex.step = FSS.Vector3.create(0.001, 0.001, 0.001);
       vertex.time = Math.randomInRange(0, Math.PIM2);
+      v--;
     }
-  }
-
-
-  // 1) Create a Renderer for the context you would like to render to.
-  //    You can use either the WebGLRenderer, CanvasRenderer or SVGRenderer.
-  var now, start = Date.now();
-  var numLights = 1;
-  var renderer = new FSS.CanvasRenderer();
-
-  // 2) Add the Renderer's element to the DOM:
-  var container = document.getElementById('container');
+  };
+  now = void 0;
+  start = Date.now();
+  numLights = 1;
+  renderer = new FSS.CanvasRenderer();
+  container = document.getElementById("container");
   container.appendChild(renderer.element);
-
-  // 3) Create a Scene:
-  var scene = new FSS.Scene();
-
-  // 4) Create some Geometry & a Material, pass them to a Mesh constructor, and add the Mesh to the Scene:
-  var geometry = new FSS.Plane( MESH.width, MESH.height, MESH.segments, MESH.slices);
-  var material = new FSS.Material('#442222', '#FFFF77');
-  var mesh = new FSS.Mesh(geometry, material);
-  initVertices(geometry)
-
-  var speed = function() { return MESH.speed / 10000.0; };
-
-  var removeLights = function(lights) {
-    lights = lights || scene.lights
+  scene = new FSS.Scene();
+  geometry = new FSS.Plane(MESH.width, MESH.height, MESH.segments, MESH.slices);
+  material = new FSS.Material("#442222", "#FFFF77");
+  mesh = new FSS.Mesh(geometry, material);
+  initVertices(geometry);
+  speed = function() {
+    return MESH.speed / 10000.0;
+  };
+  removeLights = function(lights) {
+    lights = lights || scene.lights;
     _.each(scene.lights, function(light) {
-      scene.remove(light)
+      scene.remove(light);
     });
   };
-
-  var addLights = function() {
-    oldLights = scene.lights
+  addLights = function() {
+    var oldLights;
+    oldLights = scene.lights;
     _.times(numLights, function() {
-      ambient = [ '#112222' ][ parseInt(now,10) % 1 ]
-      diffuse = [ '#ffccff' ][ parseInt(now,10) % 1 ]
-      var light = new FSS.Light(ambient, diffuse);
-      scene.add(light)
-      light.setPosition(MESH.width*Math.sin(now * speed())/4.0,
-                        MESH.height*Math.cos(now * speed())/4.0,
-                        MESH.depth * 2.0);
+      var ambient, diffuse, light, newPosition;
+      ambient = ["#112222"][parseInt(now, 10) % 1];
+      diffuse = ["#ffccff"][parseInt(now, 10) % 1];
+      light = new FSS.Light(ambient, diffuse);
+      scene.add(light);
+      newPosition = [MESH.width * Math.sin(now * speed()) / 4.0, MESH.height * Math.cos(now * speed()) / 4.0, MESH.depth * 2.0];
+      light.setPosition.apply(light, newPosition);
     });
     return oldLights;
   };
-
-  var adjustVertices = function() {
+  adjustVertices = function() {
+    var offset, ox, oy, oz, v, vertex;
     offset = MESH.depth / 2;
-    for (v = geometry.vertices.length - 1; v >= 0; v--) {
+    v = geometry.vertices.length - 1;
+    while (v >= 0) {
       vertex = geometry.vertices[v];
-
       ox = Math.sin(vertex.time + vertex.step[0] * now * speed());
       oy = Math.cos(vertex.time + vertex.step[1] * now * speed());
       oz = Math.sin(vertex.time + vertex.step[2] * now * speed());
-      FSS.Vector3.set(vertex.position,
-                      MESH.xRange*geometry.segmentWidth*ox,
-                      MESH.yRange*geometry.sliceHeight*oy,
-                      MESH.zRange*offset*oz - offset);
+      FSS.Vector3.set(vertex.position, MESH.xRange * geometry.segmentWidth * ox, MESH.yRange * geometry.sliceHeight * oy, MESH.zRange * offset * oz - offset);
       FSS.Vector3.add(vertex.position, vertex.anchor);
+      v--;
     }
-    // Set the Geometry to dirty
     geometry.dirty = true;
-  }
-
-  function animate() {
-    now = Date.now() - start;
-
-    removeLights();
-    oldLights = addLights();
-    adjustVertices();
-
-    renderer.render(scene);
-    requestAnimationFrame(animate);
-  }
-
+  };
   scene.add(mesh);
-
-  animate()
-
+  return animate();
 });
