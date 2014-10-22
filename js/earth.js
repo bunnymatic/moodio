46cf023b4b98bd5c35bd96176a4eac9260a5c233
Boram Yoon <boram@me.com>
2014-10-22 09:46:56 -0700

    Add textures

--- js/earth.js
+++ js/earth.js
@@ -6,11 +6,43 @@ var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
+var earthTexture = 'land_ocean_ice_cloud_2048.jpg';
+var mikeTexture = 'mike.jpeg';
+var kanyeTexture = 'kanye.jpg';
+var mcpTexture = 'master-control.jpg';
+var deathStarTexture = 'death-star.jpg';
+var jupiterTexture = 'jupiter.jpg';
+var moonTexture = 'moon.jpg';
+var sunTexture = 'sun.jpg';
+
+var currentMesh = null;
+var currentTexture = null;
+var firebase = new FirebaseAdapter();
+
 init();
 animate();
 
-function init() {
+function loadTexture(filename) {
+  console.log('load', filename, 'currentTexture', currentTexture);
 
+  if (filename == currentTexture)
+    return;
+
+  var loader = new THREE.TextureLoader();
+  loader.load( 'textures/' + filename, function ( texture ) {
+
+    var geometry = new THREE.SphereGeometry( 200, 30, 30 );
+    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+    var mesh = new THREE.Mesh( geometry, material );
+
+    group.remove( currentMesh );
+    group.add( mesh );
+    currentMesh = mesh;
+    currentTexture = filename;
+  });
+}
+
+function init() {
   container = document.getElementById( 'container' );
 
   camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 2000 );
@@ -20,21 +52,7 @@ function init() {
 
   group = new THREE.Object3D();
   scene.add( group );
-
-  // earth
-
-  var loader = new THREE.TextureLoader();
-  loader.load( 'textures/land_ocean_ice_cloud_2048.jpg', function ( texture ) {
-
-    var geometry = new THREE.SphereGeometry( 200, 20, 20 );
-
-    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
-    var mesh = new THREE.Mesh( geometry, material );
-    group.add( mesh );
-
-  } );
-
-  // shadow
+  loadTexture(earthTexture);
 
   var canvas = document.createElement( 'canvas' );
   canvas.width = 128;
@@ -101,10 +119,9 @@ function onDocumentMouseMove( event ) {
 
   mouseX = ( event.clientX - windowHalfX );
   mouseY = ( event.clientY - windowHalfY );
+  // console.log('mouseX', mouseX, 'mouseY', mouseY)
 
 }
-
-//
 
 function animate() {
 
@@ -117,11 +134,39 @@ function animate() {
 
 function render() {
 
-  camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-  camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
+  // camera.position.x += ( mouseX - camera.position.x ) * 0.05;
+  // camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
   camera.lookAt( scene.position );
 
-  group.rotation.y -= 0.005;
+  var currentData = firebase.read();
+  var rotationValue = 0.8;
+
+  if (currentData != null) {
+    // console.log(currentData)
+
+    if (currentData.sound) {
+      rotationValue *= currentData.sound;
+
+      if (currentData.sound > 0.02) {
+        camera.position.z = 500;
+      } else {
+        camera.position.z = 600;
+      }
+    }
+
+    if (currentData.light < 0.015) {
+      if (currentMesh != moonTexture)
+        loadTexture(moonTexture);
+    }
+
+    if (currentData.temp > 33) {
+      loadTexture(sunTexture)
+    }
+  }
+
+  // console.log(rotationValue)
+
+  group.rotation.y -= rotationValue;
 
   renderer.render( scene, camera );
 
